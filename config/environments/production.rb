0a2e7177a804d8d2171994bc66050b2a3983cdf4
Vjatseslav Gedrovits <vjatseslav.gedrovits@gmail.com>
2016-10-04 18:28:29 +0300

    Preparation for YC demo application part 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "gaming_base_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # FIXME: Use this properly
   config.action_mailer.default_url_options = { host: 'gamingbase.co' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
