339063b4ab60964cca4062710cd9228b7c100a02
gedrovits <vjatseslav.gedrovits@gmail.com>
2018-01-18 01:57:14 +0200

    Ruby 2.5.0

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') }
+  config.redis = { url: ENV.fetch('REDIS_URL', nil) }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') }
+  config.redis = { url: ENV.fetch('REDIS_URL', nil) }
 end
